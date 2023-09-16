9ebaeb618825d340fd578530ac9894f411387026
david-dev12 <david.kelly0126@gmail.com>
2023-09-16 01:48:41 -0400

    add test

--- web/src/app/components/add/add.component.spec.ts
+++ web/src/app/components/add/add.component.spec.ts
@@ -1,19 +1,1 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddComponent } from './add.component';
-
-describe('AddComponent', () => {
-  let component: AddComponent;
-  let fixture: ComponentFixture<AddComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(AddComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-});
--- web/src/app/pages/list/list.component.spec.ts
+++ web/src/app/pages/list/list.component.spec.ts
@@ -1,67 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListComponent } from './list.component';
-
 import { TravelService } from '../../services/travel.service';
-import { RouterTestingModule } from '@angular/router/testing';
-import { of, BehaviorSubject  } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Expense } from '../../interfaces/travel.interface';
-
+import { AddComponent } from "src/app/components/add/add.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Import the FontAwesomeModule
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
-  let travelService: TravelService;
+  let travelService: jasmine.SpyObj<TravelService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ListComponent],
-      imports: [RouterTestingModule],
-      providers: [TravelService],
-    })
-    .compileComponents();
+  beforeEach(() => {
+    // Create a spy object for TravelService
+    const travelServiceSpy = jasmine.createSpyObj('TravelService', [
+      'setEditData',
+      'setRemoveData',
+    ]);
+
+    TestBed.configureTestingModule({
+      declarations: [ListComponent, AddComponent],
+      providers: [{ provide: TravelService, useValue: travelServiceSpy }],
+    });
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    travelService = TestBed.inject(TravelService) as jasmine.SpyObj<TravelService>;
   });
-  
+
   it('should toggle status correctly when clicking edit and delete buttons', () => {
-    // Create a sample expense with an initial status
-    const sampleExpense = { name: 'Expense 1', amount: 100, status: 'EDITING' };
-    // Mock the expenseList$ property with a BehaviorSubject
-    const expenseListSubject = new BehaviorSubject<Expense[]>([sampleExpense]);
-    const getExpenseListSpy = spyOnProperty(travelService, 'expenseList$', 'get').and.returnValue(expenseListSubject.asObservable());
+    // Set up a sample expense list
+    const sampleExpenseList = [
+      { name: 'Expense 1', amount: 100, status: 'ACTIVE' },
+      { name: 'Expense 2', amount: 200, status: 'ACTIVE' },
+    ];
+    component.expenseList = new BehaviorSubject<Expense[]>(sampleExpenseList);
 
-
-    // Create a spy for the travelService methods
-    spyOn(travelService, 'setEditData');
-    spyOn(travelService, 'setRemoveData');
-
+    // Initialize the component
     fixture.detectChanges();
 
-    // Get the edit and delete buttons
-    const editButton = fixture.nativeElement.querySelector('.fa-edit');
-    const deleteButton = fixture.nativeElement.querySelector('.fa-trash');
+    // Simulate clicking the edit button for the first expense
+    component.edit(0);
 
-    // Click the edit button to change status to 'DONE'
-    editButton.click();
-    fixture.detectChanges();
-    expect(sampleExpense.status).toBe('DONE');
+    // Expect setEditData to be called with the correct index
+    expect(travelService.setEditData).toHaveBeenCalledWith(0);
 
-    // Click the delete button to change status to 'DELETING'
-    deleteButton.click();
-    fixture.detectChanges();
-    expect(sampleExpense.status).toBe('DELETING');
+    // Simulate clicking the delete button for the second expense
+    component.remove(1);
 
-    // Click the edit button again to change status back to 'EDITING'
-    editButton.click();
-    fixture.detectChanges();
-    expect(sampleExpense.status).toBe('EDITING');
+    // Expect setRemoveData to be called with the correct index
+    expect(travelService.setRemoveData).toHaveBeenCalledWith(1);
 
-    // Verify that the appropriate service methods were called
-    expect(travelService.setEditData).toHaveBeenCalledTimes(2);
-    expect(travelService.setRemoveData).toHaveBeenCalledTimes(1);
+    // Verify that the status has been updated in the component's expenseList
+    expect(sampleExpenseList[0].status).toBe('EDITING');
+    expect(sampleExpenseList[1].status).toBe('DELETING');
   });
-
 });

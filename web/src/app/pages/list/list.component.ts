5b3fd332257414bf0e525ab0618ab00db76c88ad
milos1223 <milos.stancik1223@gmail.com>
2023-09-16 02:13:59 +0200

    Prepare test 1

--- web/src/app/pages/list/list.component.ts
+++ web/src/app/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { faEdit, faTrash, faCheck, IconDefinition } from '@fortawesome/free-solid-svg-icons';
